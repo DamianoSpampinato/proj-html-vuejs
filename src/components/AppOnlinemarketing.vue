e22ac92cf2cdb6753cb2048daf3507b8a0090986
Damiano <ghgg585@gmail.com>
2024-04-23 00:02:53 +0200

    optimized template online marketing section

--- src/components/AppOnlinemarketing.vue
+++ src/components/AppOnlinemarketing.vue
@@ -8,7 +8,7 @@ export default{
     <section id="marketing" >    
         <div class="ms-container-md">
             <div class="row">
-                <div  class="col-4">
+                <div v-for="n in 3" class="col-4">
                     <div class="row">
                         <div class="col-2">
                             <img src="../assets/img/24.png" alt="">
@@ -16,31 +16,18 @@ export default{
                         <div class="col-10">
                             <h5>Online Marketing</h5>
                             <p>It's all a established fact that a render will be distracted</p>
+                            <ul>
+                                <li><i class="fa-solid fa-check me-2"></i> SEO</li>
+                                <li><i class="fa-solid fa-check me-2"></i> SEM</li>
+                                <li><i class="fa-solid fa-check me-2"></i> Website Strategy</li>
+                                <li><i class="fa-solid fa-check me-2"></i> Social management</li>
+        
+                            </ul>
                         </div>
                     </div>
+                    
                 </div>
-                <div  class="col-4">
-                    <div class="row">
-                        <div class="col-2">
-                            <img src="../assets/img/24.png" alt="">
-                        </div>
-                        <div class="col-10">
-                            <h5>Online Marketing</h5>
-                            <p>It's all a established fact that a render will be distracted</p>
-                        </div>
-                    </div>
-                </div>
-                <div  class="col-4">
-                    <div class="row">
-                        <div class="col-2">
-                            <img src="../assets/img/24.png" alt="">
-                        </div>
-                        <div class="col-10">
-                            <h5>Online Marketing</h5>
-                            <p>It's all a established fact that a render will be distracted</p>
-                        </div>
-                    </div>
-                </div>
+             
             </div>
             
             
@@ -49,7 +36,18 @@ export default{
 </template>
 
 <style scoped lang="scss">
+@use '../style/partials/variables' as *;
 #marketing{
     margin-top: 5rem;
+    ul{
+        
+        padding: 0;
+        li{
+            margin: 0.5rem auto;
+        }
+        .fa-solid{
+            color: $blue;
+        }
+    }
 }
 </style>
